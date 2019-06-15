f3dc3aca31ed11fb04d101d8e0e1eeeab4b2db52
Mikko <mikko.h.jokinen@gmail.com>
2019-06-15 18:39:16 +0300

    Added comments and our new king
    Just like that, yeah!

--- Assets/Scripts/ToggleButton.cs
+++ Assets/Scripts/ToggleButton.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 public class ToggleButton : MonoBehaviour
 {
     private bool toggled = false;
+    //Tells if the option is chosen or not. Should get returned in a method later with the data accompanied by the button.
     public Sprite toggledSprite;
     public Sprite untoggledSprite;
+    //Sprites for different states of the button.
 
     public void ButtonToggled ()
+    //Toggles the button on if it's off an visa versa. Also handles changing the sprites.
     {
         if (toggled)
         {
