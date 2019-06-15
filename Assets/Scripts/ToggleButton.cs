50abffb39678c3da8f114fd521f2287b5f81275a
Mikko <mikko.h.jokinen@gmail.com>
2019-06-15 17:40:06 +0300

    GameScene and stuff
    and stuff

---
+++ Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour
+{
+    private bool toggled = false;
+    public Sprite toggledSprite;
+    public Sprite untoggledSprite;
+
+    public void ButtonToggled ()
+    {
+        if (toggled)
+        {
+            toggled = false;
+            GetComponent<Image>().sprite = untoggledSprite;
+        }
+        else
+        {
+            toggled = true;
+            GetComponent<Image>().sprite = toggledSprite;
+        }
+    }
+}
