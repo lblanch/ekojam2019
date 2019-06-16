a3ad74a70fe11c57debfaa1cac07e146fa0947ba
alla.holla <alla.holla@gmail.com>
2019-06-16 12:33:25 +0300

    audio stuff added
    audio stuff

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource goodmusic;
+    public AudioSource badmusic;
+    public AudioSource normalmusic;
+
+    
+
+    void Start()
+    {
+        
+        normalmusic.Play(0);
+        goodmusic.Pause();
+        badmusic.Pause();
+        
+    }
+
+    public void playnormalmusic()
+    {
+        normalmusic.Play(0);
+        goodmusic.Pause();
+        badmusic.Pause();
+    }
+    public void playgoodmusic()
+    {
+        normalmusic.Pause();
+        goodmusic.Play(0);
+        badmusic.Pause();
+    }
+    public void playbadmusic()
+    {
+        normalmusic.Pause();
+        goodmusic.Pause();
+        badmusic.Play(0);
+    }
+  
+}
